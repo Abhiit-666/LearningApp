60031bd0b8255520e03fc52248c04e98ea8ef059
abhijit <binary.bug1111@gmail.com>
2024-09-22 21:59:51 +0530

    b: started onLoad controller; added Logger to it

---
+++ src/main/java/com/example/learningapp/Controller/OnLoadController.java
@@ -0,0 +1,36 @@
+package com.example.learningapp.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.*;
+
+import java.util.*;
+
+@RestController("api/load")
+public class OnLoadController {
+
+    private static final Logger logger= LoggerFactory.getLogger(OnLoadController.class);
+    @Autowired
+    private LoadService loadService;
+
+    @GetMapping("/fetch_top")
+    public ResponseEntity<List<Topic>> fetchtop(){
+        logger.info("Fetching the Info");
+        try{
+        List<Topic> topics=loadService.fetchtop();
+            logger.info("Data fetched");
+        }catch(Exception e){
+            logger.error("Error while loading data: "+e);
+        }
+        return ResponseEntity.ok(topics);
+    }
+
+    @GetMapping("/filter-fetch/{filter}")
+    public ResponseEntity<List<Topic>> filterfetch(@PathVariable("filter") String filter){
+        List<Topic> topics= loadService.fetchbyfilter(filter);
+        return ResponseEntity.ok(topics);
+    }
+}
